5f1aa1c0c6c9c31f490e639a3df3abe78d14dd45
kabhilan27 <kabhilan27@gmail.com>
2026-02-09 23:39:12 +0530

    Both frontend and backend working fine

---
+++ backend/config.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv() # Load environment variables from .env file
+
+# Pinecone
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_ENVIRONMENT = os.getenv("PINECONE_ENVIRONMENT","us-east-1") 
+PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME", "rag-index") 
+
+# Groq
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+# Groq model
+GROQ_MODEL_NAME = os.getenv("GROQ_MODEL_NAME", "llama-3.3-70b-versatile")
+
+# Tavily
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+
+# Embedding Model
+EMBED_MODEL = os.getenv("EMBED_MODEL", "sentence-transformers/all-MiniLM-L6-v2")
+
+# Paths (adjust as needed)
+DOC_SOURCE_DIR = os.getenv("DOC_SOURCE_DIR", "data")
